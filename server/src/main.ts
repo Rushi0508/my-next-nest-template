3c34facc20e2409db4f2b5fadf9d2037c360d823
Rushi0508 <rushigandhi14@gmail.com>
2025-04-29 13:37:05 +0530

    feat: integrate Swagger for API documentation

--- server/src/main.ts
+++ server/src/main.ts
@@ -2,6 +2,8 @@ import { NestFactory } from '@nestjs/core'
 import { AppModule } from './app.module'
 import { BadRequestException, ValidationPipe } from '@nestjs/common'
 import { GlobalExceptionFilter } from './common/filters/global-exception.filter'
+import { DocumentBuilder } from '@nestjs/swagger'
+import { SwaggerModule } from '@nestjs/swagger'
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule)
@@ -21,6 +23,17 @@ async function bootstrap() {
   )
 
   app.useGlobalFilters(new GlobalExceptionFilter())
+
+  const swaggerConfig = new DocumentBuilder()
+    .setTitle('API')
+    .setDescription('API description')
+    .setVersion('1.0')
+    .addBearerAuth()
+    .build()
+
+  const document = SwaggerModule.createDocument(app, swaggerConfig)
+  SwaggerModule.setup('docs', app, document)
+
   await app.listen(process.env.PORT ?? 3000)
 }
 bootstrap()
