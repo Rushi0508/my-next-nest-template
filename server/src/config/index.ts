db31afc2562a12babd14a2d06a74f44525346f63
Rushi0508 <rushigandhi14@gmail.com>
2025-05-18 12:49:37 +0530

    feat: integrate AWS S3 service for file upload and deletion

--- server/src/config/index.ts
+++ server/src/config/index.ts
@@ -39,4 +39,12 @@ export default () => ({
       fromName: process.env.SMTP_FROM_NAME!,
     },
   },
+  aws: {
+    s3: {
+      bucket: process.env.AWS_S3_BUCKET!,
+      region: process.env.AWS_REGION!,
+      accessKeyId: process.env.AWS_ACCESS_KEY_ID!,
+      secretAccessKey: process.env.AWS_SECRET_ACCESS_KEY!,
+    },
+  },
 })
