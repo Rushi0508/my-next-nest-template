5d2e318985dcc584dfd67a76fd1a1bc76e184533
Rushi0508 <rushigandhi14@gmail.com>
2025-05-18 12:35:25 +0530

    feat: add mail module with OTP email template and SMTP configuration

--- server/src/config/index.ts
+++ server/src/config/index.ts
@@ -25,4 +25,18 @@ export default () => ({
       },
     },
   },
+  mail: {
+    smtp: {
+      host: process.env.SMTP_HOST!,
+      port: process.env.SMTP_PORT!,
+      auth: {
+        user: process.env.SMTP_USER!,
+        pass: process.env.SMTP_PASS!,
+      },
+    },
+    defaults: {
+      from: process.env.SMTP_FROM!,
+      fromName: process.env.SMTP_FROM_NAME!,
+    },
+  },
 })
