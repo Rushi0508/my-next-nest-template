e6fbed7267bae04be4c6f5697a14c55bc9eb0fdc
Rushi0508 <rushigandhi14@gmail.com>
2025-08-02 20:19:29 +0530

    feat: add Tanstack Query for data fetching and update Providers component to include TanstackProvider

--- web/app/providers.tsx
+++ web/app/providers.tsx
@@ -5,31 +5,29 @@ import { TooltipProvider } from '@/components/ui/tooltip';
 import React from 'react';
 import { Toaster } from '@/components/ui/sonner';
 import AuthWrapper from './AuthWrapper';
-import { Poppins } from 'next/font/google';
-
-const poppins = Poppins({
-  subsets: ['latin'],
-  weight: ['100', '200', '300', '400', '500', '600', '700', '800', '900'],
-});
+import { TanstackProvider } from '@/lib/tanstack-provider';
+import { dmSans } from '@/lib/fonts';
 
 const Providers = ({ children }: { children: React.ReactNode }) => {
   return (
-    <SessionProvider>
-      <AuthWrapper>
-        <TooltipProvider>
-          <Toaster
-            toastOptions={{
-              className: poppins.className,
-            }}
-            position='top-right'
-            richColors
-            theme='light'
-            closeButton
-          />
-          {children}
-        </TooltipProvider>
-      </AuthWrapper>
-    </SessionProvider>
+    <TanstackProvider>
+      <SessionProvider>
+        <AuthWrapper>
+          <TooltipProvider>
+            <Toaster
+              toastOptions={{
+                className: dmSans.className,
+              }}
+              position='top-right'
+              richColors
+              theme='light'
+              closeButton
+            />
+            {children}
+          </TooltipProvider>
+        </AuthWrapper>
+      </SessionProvider>
+    </TanstackProvider>
   );
 };
 
---
+++ web/lib/fonts.ts
@@ -0,0 +1,11 @@
+import { DM_Sans, Poppins } from 'next/font/google';
+
+export const poppins = Poppins({
+  subsets: ['latin'],
+  weight: ['100', '200', '300', '400', '500', '600', '700', '800', '900'],
+});
+
+export const dmSans = DM_Sans({
+  subsets: ['latin'],
+  weight: ['100', '200', '300', '400', '500', '600', '700', '800', '900'],
+});
